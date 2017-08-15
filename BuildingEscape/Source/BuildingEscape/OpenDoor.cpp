dda244d282c2f8ebe58059acea6c1c4de73ec341
Trilllion <raj.karan3102@gmail.com>
2017-08-15 15:36:02 +0530

    #6

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -21,6 +21,10 @@ void UOpenDoor::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
+}
+
+void UOpenDoor::OpenDoor()
+{
 	AActor *Owner = GetOwner();
 	FRotator NewRotation = FRotator(0.0f, 45.0f, 0.0f);
 	Owner->SetActorRotation(NewRotation);
@@ -32,6 +36,13 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Poll trigger volume every frame
+	//If actor opens door overlaps
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
+	   
+		
 }
 
